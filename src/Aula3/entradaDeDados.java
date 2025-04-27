f3424d7cde38ba14c52436b828a53d074eaac8bd
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-04-27 14:03:55 -0300

    Entrada de Dados
    Entrada de Dados

---
+++ src/Aula3/entradaDeDados.java
@@ -0,0 +1,48 @@
+package Aula3;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class entradaDeDados {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		/*
+		 * String x; 
+		 * x = sc.nextLine();
+		 *  System.out.print("Você digitou " + x);
+		 */
+		
+		/*
+		 * int y; 
+		 * y = sc.nextInt(); 
+		 * System.out.println("Você digitou o número " + y);
+		 */
+		
+		/*
+		 * Double a; 
+		 * a = sc.nextDouble(); 
+		 * System.out.println("Você digitou o número " + a);
+		 */
+		
+		/*
+		 * char b; 
+		 * b = sc.next().charAt(0); 
+		 * System.out.println("A primeira letra é " +b);
+		 */
+		
+		/* String x;
+		int y;
+		double z;
+		x = sc.next();
+		y = sc.nextInt();
+		z = sc.nextDouble();
+		System.out.println("Você digitou a palavra: " + x);
+		System.out.println("Você digitou o numero inteiro: " + y);
+		System.out.println("Você digitou o número com ponto flutuante: " + z);*/
+		
+		sc.close();
+	}
+
+}
