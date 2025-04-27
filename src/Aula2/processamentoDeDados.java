10316db53093ac4ffb4031a6abef3a20461307e2
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-04-27 12:42:43 -0300

    Casting
    Casting

---
+++ src/Aula2/processamentoDeDados.java
@@ -0,0 +1,34 @@
+package Aula2;
+
+public class processamentoDeDados {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * double b = 6.0; 
+		 * double B = 8.0; 
+		 * double h = 5.0; 
+		 * double area = (b + B) / 2 * 5;
+		 */
+		 
+		/*
+		 * int a, b; 
+		 * double resultado; 
+		 * a = 5; 
+		 * b = 2;
+		 * 
+		 * resultado = (double) a/b; 
+		 * System.out.println(resultado);
+		 */
+		
+		
+		double a;
+		int b;
+		
+		a = 5.5;
+		b = (int) a;
+		
+		System.out.println(b);
+	}
+
+}
