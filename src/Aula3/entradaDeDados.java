93d25ae958bb21c600b5bb73640b3789584e76ff
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-04-28 10:33:20 -0300

    Entrada de Dados
    Entrada de Dados

--- src/Aula3/entradaDeDados.java
+++ src/Aula3/entradaDeDados.java
@@ -42,6 +42,21 @@ public class entradaDeDados {
 		System.out.println("Você digitou o numero inteiro: " + y);
 		System.out.println("Você digitou o número com ponto flutuante: " + z);*/
 		
+		
+		/*int x;
+		String s1, s2, s3;
+		x = sc.nextInt();
+		sc.nextLine();
+		s1 =sc.nextLine();
+		s2 = sc.nextLine();
+		s3 =sc.nextLine();
+		
+		System.out.println("Dados Digitados:");
+		System.out.println(x);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);*/
+		
 		sc.close();
 	}
 
