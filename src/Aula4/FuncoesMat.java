8e447f983375758a1e9cea42e9f00ae3ef86fbae
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-04-28 11:28:36 -0300

    Funções Mat
    Funções Mat

---
+++ src/Aula4/FuncoesMat.java
@@ -0,0 +1,43 @@
+package Aula4;
+
+import java.util.Locale;
+
+public class FuncoesMat {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double a, b, c;
+		
+		/*a = Math.sqrt(x);
+		b = Math.sqrt(y);
+		c = Math.sqrt(25.0);
+		System.out.println("Raiz quadrada de " + x + " = " + a);
+		System.out.println("Raiz quadrada de " + y + " = " + b);
+		System.out.println("Raiz quadrada de 25 = " + c);*/
+		
+		/*a = Math.pow(x, y);
+		b = Math.pow(x, 2.0);
+		c = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + " = " + a);
+		System.out.println(x + " elevado ao quadrado = " + b);
+		System.out.println("5 elevado ao quadrado = " + c);*/
+		
+		/*a = Math.abs(y);
+		b = Math.abs(z);
+		System.out.println("Valor absolute de " + y + " = " + a);
+		System.out.println("Valor absolute de " + z + " = " + b);*/
+		
+		b = 5;
+		a = -2;
+		c = 3;
+		double delta = Math.pow(b, 2) - 4 * a * c;
+		double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+		double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+		System.out.println(x1);
+		System.out.println(x2);
+	}
+
+}
