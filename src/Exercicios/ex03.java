fbf73150bddc3fceeb7cadea00cd0b1c3ebaf1e3
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-04-29 23:00:48 -0300

    Condicional
    Condicional

--- src/Exercicios/ex03.java
+++ src/Exercicios/ex03.java
@@ -1,10 +1,20 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		/*double num;
+		System.out.println("Digite um número:");
+		num = sc.nextDouble();
+		if (num < 0) {
+			System.out.println("NEGATIVO");
+		} else {
+			System.out.println("NÃO NEGATIVO");
+		}*/
+		
 	}
 
 }
