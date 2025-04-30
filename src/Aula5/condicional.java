09cd476d8ff833ebc5c2fcd149e3f901b3743dff
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-04-29 22:56:35 -0300

    Condicional
    Condicional

--- src/Aula5/condicional.java
+++ src/Aula5/condicional.java
@@ -15,18 +15,17 @@ public class condicional {
 			System.out.println("O Número que você digitou é menor que 5");
 		}*/
 		
-		int hora;
+		/*int hora;
 		System.out.println("Quantas horas?");
 		hora = sc.nextInt();
 		if (hora < 12) {
 			System.out.println("Bom dia!");
-		} else {
-			if (hora < 18) {
-				System.out.println("Boa tarde!");
+		} else if (hora < 18) {
+			System.out.println("Boa tarde!");
 			} else {
 				System.out.println("Boa noite!");
-			}
-		}
+			}*/
+		
 		
 		sc.close();
 
