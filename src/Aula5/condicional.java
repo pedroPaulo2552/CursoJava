b80f5f1ca319726f8a792a0a4ad1e66869d4e9b8
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-05-17 07:34:04 -0300

    Atribuição Cumulativa
    Operadores de Atribuição Cumulativa

--- src/Aula5/condicional.java
+++ src/Aula5/condicional.java
@@ -1,11 +1,13 @@
 package Aula5;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class condicional {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
 		/*int x;
 		System.out.println("Digite um número: ");
 		x = sc.nextInt();
@@ -26,6 +28,14 @@ public class condicional {
 				System.out.println("Boa noite!");
 			}*/
 		
+		System.out.println("Por quantos minutos você utilizou o telefone?");
+		int minutos = sc.nextInt();
+		double conta = 50.00;
+		if (minutos > 100) {
+			conta += (minutos - 100) * 2.00;
+		}
+		
+		System.out.printf("Valor a pagar:R$%.2f%n", conta);
 		
 		sc.close();
 
