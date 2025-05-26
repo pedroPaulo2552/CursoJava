1f0bd172a2e22ccf61649d8cec8bfce692c19d2c
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-05-26 20:02:46 -0300

    Operadores Bitwise

--- src/Aula8/OperadoresBitwise.java
+++ src/Aula8/OperadoresBitwise.java
@@ -1,10 +1,26 @@
 package Aula8;
 
+import java.util.Scanner;
+
 public class OperadoresBitwise {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		/*int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);*/
 		
-
+		int n = sc.nextInt();
+		int mask = 32;
+		if ((n & mask) == 32) {
+			System.out.println("6th bit is true!");
+		} else {
+			System.out.println("6th bit is false!");
+		}
+		
+		sc.close();
 	}
 
 }
