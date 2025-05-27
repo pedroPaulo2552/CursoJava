ecd0d63e96f83ada80e452e6df5cca3b4cf8adb1
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-05-26 21:38:22 -0300

    FunçõesString

--- src/Aula9/funcoesString.java
+++ src/Aula9/funcoesString.java
@@ -16,12 +16,9 @@ public class funcoesString {
 		
 		String s = "Potato Apple Lemon";
 		String[] vect = s.split(" ");
-		String word1 = vect[0];
-		String word2 = vect[1];
-		String word3 = vect[2];
-		System.out.println(word1);
-		System.out.println(word2);
-		System.out.println(word3);
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
 	}
 
 }
