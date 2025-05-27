176ede00becabef6fea9b8f7a38c322aaaa283a2
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-05-26 21:37:51 -0300

    Funções (Sintaxe)

---
+++ src/Aula10/funcoesSintaxe.java
@@ -0,0 +1,15 @@
+package Aula10;
+
+import java.util.Scanner;
+
+public class funcoesSintaxe {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+	}
+
+}
