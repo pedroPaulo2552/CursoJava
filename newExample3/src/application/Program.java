f2920412e969b26c537a330d9f4d39d770b71537
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-07-28 20:49:09 -0300

    Sobrecarga

---
+++ newExample3/src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		Product produto1 = new Product();
+		
+		System.out.print("Nome do produto: ");
+		produto1.name = sc.nextLine();
+		System.out.print("Preço do produto: ");
+		produto1.price = sc.nextDouble();
+		System.out.print("Quantidade em estoque: ");
+		produto1.quant = sc.nextInt();
+		
+		produto1.ShowProduct();
+		System.out.println("---------------------------------------------------");
+		System.out.println("[1] para realizar entrada no estoque");
+		System.out.println("[2] para realizar saída no estoque");
+		int res;
+		String res2;
+		do {
+			res = sc.nextInt();
+			switch (res) {
+				case 1:
+					System.out.print("Digite a quantidade de Entrada: ");
+					int ent = sc.nextInt();
+					produto1.Entrada(ent);
+					System.out.println(produto1.ShowProduct());
+					produto1.ShowProduct();
+					break;
+				
+				case 2:
+					System.out.print("Digite a quantidade de Saída: ");
+					int sai = sc.nextInt();
+					produto1.Saida(sai);
+					System.out.println(produto1.ShowProduct());
+					break;
+			};
+			System.out.print("Quer continuar? s/n: ");
+			res2 = sc.next();
+		} while (res2.equals("s"));
+		
+		
+		sc.close();
+	}
+
+}
---
+++ newExample3/src/entities/Product.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quant;
+	
+	public Product (String name, double price, int quant) {
+		this.name = name;
+		this.price = price;
+		this.quant = quant;
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public Product() {
+	}
+	
+	
+	public void Entrada(int x) {
+		quant = quant + x;
+	}
+	
+	public void Saida(int y) {
+		if (y < quant) {
+			quant = quant - y;
+		} else {
+			quant = 0;
+		}
+	}
+	
+	public double total() {
+		return quant * price;
+	}
+	
+	public String ShowProduct() {
+		return "Info do produto: " + name + ", $ " + String.format("%.2f", price) + ", " + quant + " units, Total: $ " + String.format("%.2f", total());
+	}
+}
