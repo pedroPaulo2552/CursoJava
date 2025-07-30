d9333c68a98c012ac00596640626ce0ec5b2136e
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-07-30 13:53:05 -0300

    Gerando Construtores, Getters e Setters automaticamente

---
+++ newExample5/src/entities/Product.java
@@ -0,0 +1,74 @@
+package entities;
+
+public class Product {
+	private String name;
+	private double price;
+	private int quant;
+	
+	
+	
+	public Product(String name, double price, int quant) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.quant = quant;
+	}
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+
+	public int getQuant() {
+		return quant;
+	}
+
+
+
+	public void setQuant(int quant) {
+		this.quant = quant;
+	}
+
+
+
+	public void Entrada(int x) {
+		quant = quant + x;
+	}
+	
+	public void Saida(int y) {
+		if (y < quant) {
+			quant = quant - y;
+		} else {
+			quant = 0;
+		}
+	}
+	
+	public double total() {
+		return quant * price;
+	}
+	
+	public String ShowProduct() {
+		return "Info do produto: " + name + ", $ " + String.format("%.2f", price) + ", " + quant + " units, Total: $ " + String.format("%.2f", total());
+	}
+}
