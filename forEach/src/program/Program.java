9d47caefb64f7cb8ee0978cf15a43b046c9b9c87
pedroPaulo2552 <paulopedromr2552@gmail.com>
2025-08-25 08:47:24 -0300

    For Each

---
+++ forEach/src/program/Program.java
@@ -0,0 +1,15 @@
+package program;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] vect = new String[] {"Maria", "João", "Pedro"};
+		
+		for (String obj: vect) {
+			System.out.println(obj);
+		}
+
+	}
+
+}
