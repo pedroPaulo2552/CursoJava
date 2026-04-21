c83ecbe9f4133468a8e2b1951cabc50f5d5e2747
pedroPaulo2552 <paulopedromr2552@gmail.com>
2026-04-20 21:05:41 -0300

    Exceções
    Tratando Exceções

---
+++ excecoes/src/model/entities/Account.java
@@ -0,0 +1,48 @@
+package model.entities;
+
+import model.exceptions.DomainException;
+
+public class Account {
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+	public Integer getNumber() {
+		return number;
+	}
+	
+	public String getHolder() {
+		return holder;
+	}
+	
+	public Double getBalance() {
+		return balance;
+	}
+	
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+	
+	
+	public void withdraw(Double amount) throws DomainException {
+		if (amount > withdrawLimit) {
+			throw new DomainException("The amount exceeds withdraw limit");
+		}
+		if (amount > balance) {
+			throw new DomainException("Not enough balance");
+		}
+		balance -= amount;
+	}
+	@Override
+	public String toString() {
+		return "New balance: " + String.format("%.2f", getBalance());
+	}
+	
+	
+}
