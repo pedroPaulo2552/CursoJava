7fbdbdc0ffc8b051fff0291daeb47afeb1cf096c
pedroPaulo2552 <paulopedromr2552@gmail.com>
2026-06-22 17:41:21 -0300

    HashCode e Equals

---
+++ HashAndEquals/src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import entities.Client;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Client c1 = new Client("Maria", "mama@gmail.com");
+		Client c2 = new Client("Maria", "mama@gmail.com");
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1 == c2);
+	}
+
+}
